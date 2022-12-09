62fbde00818608074f54627ad39c3bba83792ee6
tsubosak <koseit+git@googlegroups.com>
2022-12-09 16:27:34 +0900

    :sparkles: Add (broken) swagger API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 import { AppModule } from "./app.module"
+import { patchNestjsSwagger } from "@abitia/zod-dto"
 
 async function bootstrap() {
+  patchNestjsSwagger()
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle("kizuna-music API")
+    .setVersion("2.0")
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("", app, document)
+
   await app.listen(7000)
 }
 bootstrap()
