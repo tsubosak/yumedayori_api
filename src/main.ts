8406e67f50e3c78c292b888533d7e4b8585e5bac
tsubosak <koseit+git@googlegroups.com>
2022-12-15 01:57:01 +0900

    :art: enable cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup("", app, document)
 
+  app.enableCors()
+
   await app.listen(7000)
 }
 bootstrap()
